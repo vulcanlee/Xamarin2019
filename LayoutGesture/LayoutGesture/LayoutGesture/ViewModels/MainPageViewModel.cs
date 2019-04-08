8a486b86afc6a125c014291eb37d1cf35464b29b
李進興 Vulcan <vulcan@miniasp.com>
2019-04-08 10:18:48 +0800

    Xamarin.Forms 的控制項與版面配置的手勢操作

---
+++ LayoutGesture/LayoutGesture/LayoutGesture/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LayoutGesture.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DelegateCommand<string> TapCommand { get; set; }
+        private readonly INavigationService navigationService;
+        public string Hint { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            TapCommand = new DelegateCommand<string>(x =>
+            {
+                Hint = x;
+            });
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}

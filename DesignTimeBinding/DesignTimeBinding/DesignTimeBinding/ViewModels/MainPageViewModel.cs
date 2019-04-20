2b0009210829246797bac757c346ca11858a9312
李進興 Vulcan <vulcan@miniasp.com>
2019-04-20 09:36:01 +0800

    Xamarin.Forms 之 XAML 設計預覽

---
+++ DesignTimeBinding/DesignTimeBinding/DesignTimeBinding/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignTimeBinding.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Message { get; set; }
+        private readonly INavigationService navigationService;
+        public MainPageViewModel()
+        {
+            Message = "Come From Default Constuctor  !!";
+        }
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            Message = "Come From Injection Constructor";
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}

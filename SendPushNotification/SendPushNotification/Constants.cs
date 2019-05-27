1bdcc5528f60a53b312cf38837dd1dced75f6d54
李進興 Vulcan <vulcan@miniasp.com>
2019-05-27 17:38:26 +0800

    使用 PushSharp 來推送 Push Notification 到 Xamarin.Forms 開發的 Android 或者 iOS App 上

---
+++ SendPushNotification/SendPushNotification/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendPushNotification
+{
+    public class Constants
+    {
+        public static string APNS_P12FILENAME { get; set; } = "com.vulcan.azurehub.p12";
+        public static string PUSH_CERT_PWD { get; set; } = "";
+        public static string GCM_SENDER_ID { get; set; } = "";
+        public static string AUTH_TOKEN { get; set; } = "";
+    }
+}
---
+++ SendPushNotification/SendPushNotification/Program.cs
@@ -0,0 +1,186 @@
+﻿using Newtonsoft.Json.Linq;
+using PushSharp.Apple;
+using PushSharp.Core;
+using PushSharp.Google;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendPushNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 進行 iOS 裝置的推播
+            PushiOS("ad1942af16e2d85d40fbc7e186555eb276ce67a8112fe5fbe0b6b33a1d404a3f");
+            ////進行 Android 裝置的推播
+            //PushAndroid("eJTHqO53Tbk:APA91bGUWCleYTdY39Ws1JmbtMnJm80RRsWm8sE5zINsd5YP4dDi_lqp1MHnD38Hr-x7UpIdx0VP3TEuOgpQ7W77bE_c0k4G2FGLK73GAiHsdqEJZhyF8rBIjC7hmfBHOoqteE_cWCbC");
+        }
+
+        static void PushiOS(string deviceid)
+        {
+            // Configuration (NOTE: .pfx can also be used here)
+            var config = new ApnsConfiguration(ApnsConfiguration.ApnsServerEnvironment.Sandbox,
+                Constants.APNS_P12FILENAME, Constants.PUSH_CERT_PWD);
+
+            // Create a new broker
+            var apnsBroker = new ApnsServiceBroker(config);
+
+            // Wire up events
+            apnsBroker.OnNotificationFailed += (notification, aggregateEx) =>
+            {
+
+                aggregateEx.Handle(ex =>
+                {
+
+                    // See what kind of exception it was to further diagnose
+                    if (ex is ApnsNotificationException notificationException)
+                    {
+
+                        // Deal with the failed notification
+                        var apnsNotification = notificationException.Notification;
+                        var statusCode = notificationException.ErrorStatusCode;
+
+                        Console.WriteLine($"Apple Notification Failed: ID={apnsNotification.Identifier}, Code={statusCode}");
+
+                    }
+                    else
+                    {
+                        // Inner exception might hold more useful information like an ApnsConnectionException			
+                        Console.WriteLine($"Apple Notification Failed for some unknown reason : {ex.InnerException}");
+                    }
+
+                    // Mark it as handled
+                    return true;
+                });
+            };
+
+            apnsBroker.OnNotificationSucceeded += (notification) =>
+            {
+                Console.WriteLine("Apple Notification Sent!");
+            };
+
+            // Start the broker
+            apnsBroker.Start();
+
+            string sendMessage = File.ReadAllText("iOS.json", System.Text.Encoding.UTF8);
+
+            // Queue a notification to send
+            apnsBroker.QueueNotification(new ApnsNotification
+            {
+                DeviceToken = deviceid,
+                Payload = JObject.Parse(sendMessage)
+            });
+
+            // Stop the broker, wait for it to finish   
+            // This isn't done after every message, but after you're
+            // done with the broker
+            apnsBroker.Stop();
+        }
+
+        static void PushAndroid(string regId)
+        {
+            // Configuration GCM (use this section for GCM)
+            var config = new GcmConfiguration(Constants.GCM_SENDER_ID, Constants.AUTH_TOKEN, null);
+            var provider = "GCM";
+
+            // Configuration FCM (use this section for FCM)
+            // var config = new GcmConfiguration("APIKEY");
+            // config.GcmUrl = "https://fcm.googleapis.com/fcm/send";
+            // var provider = "FCM";
+
+            // Create a new broker
+            var gcmBroker = new GcmServiceBroker(config);
+
+            // Wire up events
+            gcmBroker.OnNotificationFailed += (notification, aggregateEx) =>
+            {
+
+                aggregateEx.Handle(ex =>
+                {
+
+                    // See what kind of exception it was to further diagnose
+                    if (ex is GcmNotificationException notificationException)
+                    {
+
+                        // Deal with the failed notification
+                        var gcmNotification = notificationException.Notification;
+                        var description = notificationException.Description;
+
+                        Console.WriteLine($"{provider} Notification Failed: ID={gcmNotification.MessageId}, Desc={description}");
+                    }
+                    else if (ex is GcmMulticastResultException multicastException)
+                    {
+
+                        foreach (var succeededNotification in multicastException.Succeeded)
+                        {
+                            Console.WriteLine($"{provider} Notification Succeeded: ID={succeededNotification.MessageId}");
+                        }
+
+                        foreach (var failedKvp in multicastException.Failed)
+                        {
+                            var n = failedKvp.Key;
+                            var e = failedKvp.Value;
+
+                            Console.WriteLine($"{provider} Notification Failed: ID={n.MessageId}, Desc={e.Message}");
+                        }
+
+                    }
+                    else if (ex is DeviceSubscriptionExpiredException expiredException)
+                    {
+
+                        var oldId = expiredException.OldSubscriptionId;
+                        var newId = expiredException.NewSubscriptionId;
+
+                        Console.WriteLine($"Device RegistrationId Expired: {oldId}");
+
+                        if (!string.IsNullOrWhiteSpace(newId))
+                        {
+                            // If this value isn't null, our subscription changed and we should update our database
+                            Console.WriteLine($"Device RegistrationId Changed To: {newId}");
+                        }
+                    }
+                    else if (ex is RetryAfterException retryException)
+                    {
+
+                        // If you get rate limited, you should stop sending messages until after the RetryAfterUtc date
+                        Console.WriteLine($"{provider} Rate Limited, don't send more until after {retryException.RetryAfterUtc}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("{provider} Notification Failed for some unknown reason");
+                    }
+
+                    // Mark it as handled
+                    return true;
+                });
+            };
+
+            gcmBroker.OnNotificationSucceeded += (notification) =>
+            {
+                Console.WriteLine("{provider} Notification Sent!");
+            };
+
+            // Start the broker
+            gcmBroker.Start();
+
+            string sendMessage = File.ReadAllText("Android.json", System.Text.Encoding.UTF8);
+
+            // Queue a notification to send
+            gcmBroker.QueueNotification(new GcmNotification
+            {
+                RegistrationIds = new List<string> { regId },
+                Data = JObject.Parse(sendMessage)
+            });
+
+            // Stop the broker, wait for it to finish   
+            // This isn't done after every message, but after you're
+            // done with the broker
+            gcmBroker.Stop();
+        }
+    }
+}

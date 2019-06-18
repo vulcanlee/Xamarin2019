89c18565ae3c7562e3b3b687237b26d4519b77af
李進興 Vulcan <vulcan@miniasp.com>
2019-06-18 13:55:44 +0800

    如何使用 On_PropertyName_Changed 與 附加屬性 Attached Properties 和行為 Behavior，來得知 Entry 與 Editor 已經輸入了多少文字

---
+++ XFCharCount/XFCharCount/XFCharCount/CharCountBehavior.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFCharCount
+{
+    public class CharCountAttachedProperty
+    {
+        #region EnableCharCount 附加屬性 Attached Property
+        public static readonly BindableProperty EnableCharCountProperty =
+               BindableProperty.CreateAttached(
+                   propertyName: "EnableCharCount",   // 屬性名稱 
+                   returnType: typeof(bool), // 回傳類型 
+                   declaringType: typeof(View), // 宣告類型 
+                   defaultValue: false, // 預設值 
+                   propertyChanged: OnEnableCharCountChanged  // 屬性值異動時，要執行的事件委派方法
+               );
+
+        public static void SetEnableCharCount(BindableObject bindable, bool entryType)
+        {
+            bindable.SetValue(EnableCharCountProperty, entryType);
+        }
+        public static bool GetEnableCharCount(BindableObject bindable)
+        {
+            return (bool)bindable.GetValue(EnableCharCountProperty);
+        }
+
+        private static void OnEnableCharCountChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is Entry || bindable is Editor)
+            {
+                bool isEnable = (bool)newValue;
+                if (isEnable)
+                {
+                    if (bindable is Entry)
+                    {
+                        (bindable as Entry).TextChanged += OnEntryTextChanged;
+                    }
+                    else if (bindable is Editor)
+                    {
+                        (bindable as Editor).TextChanged += OnEntryTextChanged;
+                    }
+                }
+                else
+                {
+                    if (bindable is Entry)
+                    {
+                        (bindable as Entry).TextChanged -= OnEntryTextChanged;
+                    }
+                    else if (bindable is Editor)
+                    {
+                        (bindable as Editor).TextChanged -= OnEntryTextChanged;
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+        #endregion
+        private static void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            int foo = CharCountAttachedProperty.GetCharNumber(sender as BindableObject);
+            int length = e.NewTextValue.Length;
+            CharCountAttachedProperty.SetCharNumber(sender as BindableObject, length);
+            (sender as BindableObject).SetValue(CharNumberProperty, length);
+            int foo2 = CharCountAttachedProperty.GetCharNumber(sender as BindableObject);
+        }
+
+        #region CharNumber 附加屬性 Attached Property
+        public static readonly BindableProperty CharNumberProperty =
+               BindableProperty.CreateAttached(
+                   propertyName: "CharNumber",   // 屬性名稱 
+                   returnType: typeof(int), // 回傳類型 
+                   declaringType: typeof(View), // 宣告類型 
+                   defaultValue: 0, // 預設值 
+                   propertyChanged: OnCharNumberChanged  // 屬性值異動時，要執行的事件委派方法
+               );
+
+        public static void SetCharNumber(BindableObject bindable, int entryType)
+        {
+            bindable.SetValue(CharNumberProperty, entryType);
+        }
+        public static int GetCharNumber(BindableObject bindable)
+        {
+            return (int)bindable.GetValue(CharNumberProperty);
+        }
+
+        private static void OnCharNumberChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+        }
+
+        #endregion
+
+    }
+}
---
+++ XFCharCount/XFCharCount/XFCharCount/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XFCharCount.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string EditorInputText { get; set; }
+        public int EditorInputTextLength { get; set; } 
+        public string EntryInputInputText { get; set; }
+        public int EntryInputTextLength { get; set; } 
+        public string EntryChangedInputText { get; set; }
+        public int EntryChangedInputTextLength { get; set; } 
+        public string EntryBehaviorInputText { get; set; }
+        public int EntryBehaviorInputTextLength { get; set; }
+        public DelegateCommand EntryChangedCommand { get; set; }
+        private readonly INavigationService navigationService;
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            EntryChangedCommand = new DelegateCommand(() =>
+            {
+                EntryBehaviorInputTextLength = EntryBehaviorInputText is null ? 0 : EntryBehaviorInputText.Length;
+            });
+        }
+        public void OnEntryChangedInputTextChanged()
+        {
+            if(string.IsNullOrEmpty(EntryChangedInputText))
+            {
+                EntryChangedInputTextLength = 0;
+            }
+            else
+            {
+                EntryChangedInputTextLength = EntryChangedInputText.Length;
+            }
+        }
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
